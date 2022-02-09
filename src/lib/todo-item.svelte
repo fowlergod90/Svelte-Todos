c3bceef8b3a659f75dd82963fa6c706e4a9a59f0
fowlergod90 <fowler.god90@gmail.com>
2022-02-09 03:03:35 +0000

    style todo item

--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -1,15 +1,31 @@
-<div>
+<style>
+    .todo {
+        display: grid;
+        grid-template-columns: 2rem 1fr 2rem;
+        gap: 0.5rem;
+        align-items: center;
+        margin: 0 0 0.5rem 0;
+        padding: 0.5rem;
+        background-color: white;
+        border-radius: 8px;
+        filter: drop-shadow(2px 4px 6px rgba(0,0,0,0.1));
+        transform: translate(-1px,-1px);
+        transition: filter 0.2s transform 0.2s;
+    }
+</style>
+
+<div class="todo">
     <form action="" method="" >
         <input type="hidden" name="done" value="" />
-        <button aria-label="Mark done/undone">Done/Undone</button>    
+        <button aria-label="Mark done/undone" class="toggle">Done/Undone</button>    
     </form>
 
     <form action="" method="">
         <input type="text"/>
-        <button aria-label="Save todo">Save</button>
+        <button aria-label="Save todo" class="save">Save</button>
     </form>
     
     <form action="" method="">
-        <button aria-label="Delete todo">Delete</button>
+        <button aria-label="Delete todo" class="delete">Delete</button>
     </form>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -17,8 +17,28 @@
 
     .new input{
         font-size : 28px;
+        width : 100%;
+        padding: 0.5em 1em 0.3em 1em;
+        box-sizing: border-box;
+        background: rgba(255, 255, 255, 0.05);
+        border-radius: 8px;
+        text-align: center;
     }
 
+    /* this is to target to removal border from all input fields in class todos and its child component */
+    /* i.e. within TodoItem.  This is svelte way. */
+
+    .todos :global(input){
+        border: 1px solid transparent;
+    }
+
+    /* this is to removal borer when its focus - svelte way */
+
+    .todos :global(input:focus-visible){
+        box-shadow: inset 1px 1px 1px 6px rgba(0, 0, 0, 0.1);
+        border: 1px solid #ff3e00 !important;
+        outline: none;
+    }
 </style>
 
 <svelte:head>
