c03505ac236e36bb58622ea238cb55a020a7059c
fowlergod90 <fowler.god90@gmail.com>
2022-02-11 10:00:48 +0000

    fix hooks-3

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,7 +4,7 @@ export async function handle({ event, resolve }) {
 
     console.log("hooks");
     console.log(event.params);
-    console.log(event.request.url.searchParams);
+    console.log(event.url.searchParams.get('_mt'));
     console.log(event.request.method);
 //    console.log(event);
 	const response = await resolve(event);
