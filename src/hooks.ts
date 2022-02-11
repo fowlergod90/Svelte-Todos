338d5dec95a230e3a6e3106262b85483d7979951
fowlergod90 <fowler.god90@gmail.com>
2022-02-11 09:31:34 +0000

    hooks-2

--- src/hooks.ts
+++ src/hooks.ts
@@ -4,9 +4,9 @@ export async function handle({ event, resolve }) {
 
     console.log("hooks");
     console.log(event.params);
-    console.log(event.request.url);
+    console.log(event.request.url.searchParams);
     console.log(event.request.method);
-
+//    console.log(event);
 	const response = await resolve(event);
 
 	return response;
--- src/lib/todo-item.svelte
+++ src/lib/todo-item.svelte
@@ -85,7 +85,7 @@
         <button aria-label="Save todo" class="save"></button>
     </form>
     
-    <form action="/todos/{todo.uid}" method="delete">
+    <form action="/todos/{todo.uid}?_mt=del" method="post">
         <button aria-label="Delete todo" class="delete"></button>
     </form>
 </div>
